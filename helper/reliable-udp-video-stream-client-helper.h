37fdb630081a88cd91a0d9b616c5e8ab5d580641
KLDAJI <kldaji@naver.com>
2022-05-27 21:38:23 +0900

    ReliableUdpVideoStreamClientHelper.h -> reliable-udp-video-stream-client-helper.h

---
+++ helper/reliable-udp-video-stream-client-helper.h
@@ -0,0 +1,11 @@
+#ifndef RELIABLE_UDP_VIDEO_STREAM_CLIENT_HELPER_H
+#define RELIABLE_UDP_VIDEO_STREAM_CLIENT_HELPER_H
+
+#include <stdint.h>
+#include "ns3/application-container.h"
+#include "ns3/node-container.h"
+#include "ns3/object-factory.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/reliable-udp-video-stream-client.h"
+
+#endif /* RELIABLE_UDP_VIDEO_STREAM_CLIENT_HELPER_H */
