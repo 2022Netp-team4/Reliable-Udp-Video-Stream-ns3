f8e4d6296474a198d132e9291ed89a5345736ad5
KLDAJI <kldaji@naver.com>
2022-05-27 20:09:29 +0900

    class 생성

--- model/ReliableUdpVideoStreamClient.h
+++ model/ReliableUdpVideoStreamClient.h
@@ -7,4 +7,22 @@
 #include "ns3/address.h"
 #include "ns3/traced-callback.h"
 
+namespace ns3 {
+/**
+ * \ingroup applications
+ * \defgroup reliableudpvideostreamclient ReliableUdpVideoStreamClient
+ */
+
+/**
+ * \ingroup reliableudpvideostreamclient
+
+ * \brief A UDP client, receives UDP packets from a remote server in a reliable way.
+ 
+ * UDP packets are divided from frames and enqueued to particular queue when * they are not a retransmitted one. The retransmitted packets are enqueued  * to the other particular queue for reliability. 
+ */
+class ReliableUdpVideoStreamClient : public Application
+{
+
+};
+} // namespace ns3
 #endif /* RELIABLE_UDP_VIDEO_STREAM_CLIENT_H */
