f83d8368e935a77edbcd30478ce26165de57edce
KLDAJI <kldaji@naver.com>
2022-05-27 19:48:18 +0900

    #include 추가

--- model/ReliableUdpVideoStreamClient.h
+++ model/ReliableUdpVideoStreamClient.h
@@ -1,4 +1,10 @@
 #ifndef RELIABLE_UDP_VIDEO_STREAM_CLIENT_H
 #define RELIABLE_UDP_VIDEO_STREAM_CLIENT_H
 
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/traced-callback.h"
+
 #endif /* RELIABLE_UDP_VIDEO_STREAM_CLIENT_H */
