d74d62610e3f335f3f5b8f15a7e17c1df1e0fedd
seungwookim <seungwookim.dev@gmail.com>
2022-05-30 14:04:48 +0900

    Update: 로그 컴포넌트 및 기본 TypeId 지정

--- scratch/reliable-udp-server.cc
+++ scratch/reliable-udp-server.cc
@@ -4,9 +4,25 @@
 #include "ns3/address.h"
 #include "ns3/traced-callback.h"
 #include "ns3/queue.h"
-#include "ns3/reliable-udp-server.h"
-#include <map>
+
+#include "reliable-udp-server.h
 
 namespace ns3 {
 
+    NS_LOG_COMPONENT_DEFINE ("ReliableUdpServerApplication");
+
+    NS_OBJECT_ENSURE_REGISTERED (ReliableUdpServer);
+
+    TypeId
+    ReliableUdpServer::GetTypeId(void) {
+        static TypeId tid = TypeId("ns3::ReliableUdpServer")
+                .SetParent<Application>()
+                .SetGroupName("Applications")
+                .AddConstructor<ReliableUdp>()
+                .AddAttribute("Port", "Port on which we listen for incoming packets.",
+                              UintegerValue(9),
+                              MakeUintegerAccessor(&UdpEchoServer::m_port),
+                              MakeUintegerChecker<uint16_t>());
+        return tid;
+    }
 }
