6d541209327d614e72fc157d947c15dcde1a77c3
seungwookim <seungwookim.dev@gmail.com>
2022-05-30 15:49:17 +0900

    Update: Application Start/Stop, Constructors

--- scratch/reliable-udp-server.cc
+++ scratch/reliable-udp-server.cc
@@ -4,8 +4,9 @@
 #include "ns3/address.h"
 #include "ns3/traced-callback.h"
 #include "ns3/queue.h"
+#include <map>
 
-#include "reliable-udp-server.h
+#include "reliable-udp-server.h"
 
 namespace ns3 {
 
@@ -21,8 +22,58 @@ namespace ns3 {
                 .AddConstructor<ReliableUdp>()
                 .AddAttribute("Port", "Port on which we listen for incoming packets.",
                               UintegerValue(9),
-                              MakeUintegerAccessor(&UdpEchoServer::m_port),
+                              MakeUintegerAccessor(&ReliableUdpServer::m_port),
                               MakeUintegerChecker<uint16_t>());
         return tid;
+    }
+
+    ReliableUdpServer::ReliableUdpServer() {
+        NS_LOG_FUNCTION(this);
+    }
+
+    ReliableUdpServer::~ReliableUdpServer() {
+        NS_LOG_FUNCTION(this);
+        m_socket = 0;
+    }
+
+    void
+    ReliableUdpServer::DoDispose(void) {
+        NS_LOG_FUNCTION(this);
+        Application::DoDispose();
+    }
+
+    void
+    ReliableUdpServer::StartApplication(void) {
+        NS_LOG_FUNCTION(this);
+
+        if (m_socket == 0) {
+            TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
+            m_socket = Socket::CreateSocket(GetNode(), tid);
+            InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), m_port);
+            if (m_socket->Bind(local) == -1) {
+                NS_FATAL_ERROR("Failed to bind socket");
+            }
+            if (addressUtils::IsMulticast(m_local)) {
+                Ptr <UdpSocket> udpSocket = DynamicCast<UdpSocket>(m_socket);
+                if (udpSocket) {
+                    // equivalent to setsockopt (MCAST_JOIN_GROUP)
+                    udpSocket->MulticastJoinGroup(0, m_local);
+                } else {
+                    NS_FATAL_ERROR("Error: Failed to join multicast group");
+                }
+            }
+        }
+
+        m_socket->SetRecvCallback(MakeCallback(&ReliableUdpServer::HandleRead, this));
+    }
+
+    void
+    ReliableUdpServer::StopApplication() {
+        NS_LOG_FUNCTION(this);
+
+        if (m_socket != 0) {
+            m_socket->Close();
+            m_socket->SetRecvCallback(MakeNullCallback < void, Ptr < Socket > > ());
+        }
     }
 }
